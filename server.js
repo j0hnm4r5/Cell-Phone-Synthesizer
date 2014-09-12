d18e1c24f7275a29293d902169367c3277e34a44
John Mars <jmars764@gmail.com>
2014-09-11 23:29:25 -0400

    Add server

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express'),
+	http = require('http'),
+	app = express();
+
+// Tell express to load any file in the public folder
+app.use(express.static(__dirname + '/public'));
+
+// Launch server
+var server = app.listen(8000, function() {
+	console.log('Listening on port %d', server.address().port);
+});
